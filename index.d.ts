1fbd7f8b7bda24ce5d271c3201d746379db9ec4b
Enoch Klu <eenoch@delalify.com>
2023-10-12 10:05:48 +0000

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+export interface CSVReaderOptions {
+  delimiter?: "," | (string & {});
+  headers?: boolean;
+  readableObjectMode?: boolean;
+}
+
+class CSVReader extends Transform {
+  options: CSVReaderOptions;
+  buffer: string;
+  queue: string[];
+  headers: unknonw[] | null;
+  Item: Record<string, string>;
+  firstWrite: boolean;
+  lineCount: 0;
+
+  constructor(options?: CSVReaderOptions): CSVReader;
+
+  addToQueue(lines: string[], callback: (line: null) => void): void;
+  emitLine(line: string, callback: (error: string | null) => void): void;
+}
+
+export interface Reader extends CSVReader {}
+
+export declare function load(filename: string, options?: CSVReaderOptions): Promise<Array<Record<string, string>>>;
+
+export declare function save(
+  filename: string,
+  data: Array<Record<string, string>>,
+  keys?: Array<string>
+): Promise<void>;
+
+export declare function writeLine(out: string, keys: Array<string>, data?: Record<string, string>): boolean;
